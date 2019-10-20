5a784e47b8d53c58934bb95243b5a505bc63c33d
Rafael Simões <rafaeljsimoes@gmail.com>
2019-10-20 19:47:29 +0100

    Models done

--- rest_api/api/models.py
+++ rest_api/api/models.py
@@ -1,25 +1,54 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from rest_api.settings import MAX_JOURNEY, MIN_JOURNEY
 
 
 # Create your models here.
-class Player_position(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=1000)
 
 
-class Stadium(object):
+class Stadium(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=1000, unique=True)
-    address = models.CharField(max_length=1000, unique=True)
-    spot_numbers = models.IntegerField()  # Fazer o check???
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    number_seats = models.IntegerField(
+        validators=[
+            MinValueValidator(0)
+        ]
+    )
+    picture = models.ImageField(null=True, blank=True)
 
 
 class Team(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=1000, unique=True)
-    creation_data = models.DateField(blank=True)
-    stadium_id = models.ForeignKey(Stadium)
-    logo = models.TextField(blank=True)
+    name = models.CharField(max_length=200)
+    foundation_date = models.DateField()
+    logo = models.ImageField(null=True, blank=True)
+    stadium = models.OneToOneRel(Stadium)
+
+
+class Game(models.Model):
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField()
+    journey = models.IntegerField(validators=[
+        MaxValueValidator(MAX_JOURNEY),
+        MinValueValidator(MIN_JOURNEY)
+    ])
+    stadium = models.ManyToOneRel(Stadium)
+    team = models.ManyToManyField(Team)
+    shots = models.IntegerField(validators=[
+        MinValueValidator(0)
+    ])
+    ball_possession = models.IntegerField(validators=[
+        MaxValueValidator(100),
+        MinValueValidator(0)
+    ])
+    corners = models.IntegerField(validators=[
+        MinValueValidator(0)
+    ])
+
+class Player_position(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1000)
 
 
 class Player(models.Model):
@@ -32,14 +61,9 @@ class Player(models.Model):
     team = models.ManyToManyField(Team)
 
 
-class Game(object):
-    pass
-
-
 class Player_function(object):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=1000)
-
 
 class Player_play_game(models.Model):
     player_id = models.ForeignKey(Player, primary_key=True)
@@ -57,3 +81,4 @@ class Event(models.Model):
     player_id = models.ForeignKey(Player, primary_key=True)
     game_id = models.ForeignKey()
     event_id = models.ForeignKey(Kind_event)
+
