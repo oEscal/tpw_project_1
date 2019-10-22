0e1acfd251eb407c49f04326377b16fab86fc0de
oEscal <pedroescaleira@hotmail.com>
2019-10-22 18:33:56 +0100

    no rest

--- rest_api/position_isert.sql
+++
@@ -1,4 +0,0 @@
-insert into api_position values(1,"Guarda-redes");
-insert into api_position values(2,"Defesa");
-insert into api_position values(3,"Médio");
-insert into api_position values(4,"Avançado");
