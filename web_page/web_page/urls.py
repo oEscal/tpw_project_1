8ae530f8b4f7b4d7a1b9d2b34fb059e61d00deba
oEscal <pedroescaleira@hotmail.com>
2019-11-05 11:52:27 +0000

    edit players playing game button added to the games page

--- web_page/web_page/urls.py
+++ web_page/web_page/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     url(r'^update_team/(?P<name>[\w\s]+)/$', update_team, name='update_team'),
     url(r'^update_game/(?P<id>\w+)/$', update_game, name='update_game'),
     url(r'^update_stadium/(?P<name>[\w\s]+)/$', update_stadium, name='update_stadium'),
-    url(r'^update_players_game/(?P<id>\w+)/$', update_player_game, name='update_team'),
+    url(r'^update_players_game/(?P<id>\w+)/$', update_player_game, name='update_players_game'),
     url(r'^update_player/(?P<id>[\w\s]+)/$', update_player, name='update_player'),
     url(r'^update_event/(?P<id>[\w\s]+)/$', update_event, name='update_event'),
 ]
